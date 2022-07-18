3ada60068adfe13360ddac33db5b2433c332f50c
hkagei <k_dawg_1022@hotmail.com>
2022-07-18 12:21:47 -0600

    20.1.5

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import About from './components/About'
+import coverImage from "../../assets/cover/cover-image.jpg";
 
 function About() {
     return (
-        <section>
-            <h1 id="About">Who am I?</h1>
+        <section className='my-5'>
+            <h1 id="about">Who am I?</h1>
+            <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
         </section>
+        
     );
 }
 

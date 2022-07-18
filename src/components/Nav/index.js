6605557857a6c002afa9f586d3d265fc4b57ee6d
hkagei <k_dawg_1022@hotmail.com>
2022-07-18 12:40:57 -0600

    20.1.6

---
+++ src/components/Nav/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <header>
+      <h2>
+        <a href="/">
+          <span role="img" aria-label="camera">
+            {" "}
+            📸
+          </span>{" "}
+          Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about">About me</a>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+          function categorySelected(name) {
+          console.log(`${name} clicked`)
+          }. . .
+          {categories.map((category) => (
+            <li className="mx-1" key={category.name}>
+              <span onClick={() => categorySelected(category.name)}>
+                {category.name}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
